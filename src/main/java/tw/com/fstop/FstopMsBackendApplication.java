1d6e78a27204f59ce1ff220951d4a715e8b29af2
andy <andy.udp.ip@gmail.com>
2017-08-10 20:01:50 +0800

    first commit

---
+++ src/main/java/tw/com/fstop/FstopMsBackendApplication.java
@@ -0,0 +1,12 @@
+package tw.com.fstop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FstopMsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FstopMsBackendApplication.class, args);
+	}
+}
